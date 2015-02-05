ab4efbce51c8326b768e119760c054155a9573d7
Adrian Greeve <adrian@moodle.com>
2015-02-05 14:22:34 +0800

    MDL-45894 navigation: My grades update to overview report.
    Part of MDL-45774

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -4216,5 +4216,13 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2015030400.00);
     }
 
+    if ($oldversion < 2015030400.00) {
+        // Add "My grades" to the user menu.
+        $newconfig = "mygrades,grades|/grade/report/mygrades.php|grades\n" . $CFG->customusermenuitems;
+        set_config('customusermenuitems', $newconfig);
+
+        upgrade_main_savepoint(true, 2015030400.00);
+    }
+
     return true;
 }
